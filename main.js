3decbaf73f1e956ce80201333ccfacb5e39b8a34
opalecky <adam@opalecky.cz>
2023-09-15 18:51:16 +0200

    Add twitch and ign fields

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const ELEMENTS = {
     LINK: document.querySelector('input[name="gamelink"]'),
+    TWITCH: document.querySelector('input[name="twitchname"]'),
+    IGN: document.querySelector('input[name="ign"]'),
     MODE: document.querySelectorAll('input[name="game-type"]'),
     COLOR: document.querySelectorAll('input[name="game-color"]'),
     MESSAGE: document.querySelector('textarea[name="personalmessage"]'),
@@ -27,12 +29,28 @@ function update() {
     let upLink = updateOutput('gamelink', link);
     let upMode = updateOutput('gametype', mode.toUpperCase(), upLink);
     let upColor = updateOutput('gamecolor', color.toUpperCase(), upMode);
-    ELEMENTS.OUTPUT.innerHTML = updateOutput('PersonalMessage', message, upColor);
+
+
+    let upMessage = updateOutput('PersonalMessage', message, upColor);
+    
+    if (ELEMENTS.TWITCH.value != '') {
+        const twitch = ELEMENTS.TWITCH.value;
+        upMessage += "<br><br>Twitch name: **" + twitch + "**";
+    }
+    
+    if (ELEMENTS.IGN.value != '') {
+        const ign = ELEMENTS.IGN.value;
+        upMessage += "<br>In Game Name: **" + ign + "**";
+    }
+
+    ELEMENTS.OUTPUT.innerHTML = upMessage;
 
 }
 
 function main() {
     ELEMENTS.LINK.addEventListener('input', update);
+    ELEMENTS.TWITCH.addEventListener('input', update);
+    ELEMENTS.IGN.addEventListener('input', update);
     for (const el of ELEMENTS.MODE) el.addEventListener('change', update);
     for (const el of ELEMENTS.COLOR) el.addEventListener('change', update);
     ELEMENTS.MESSAGE.addEventListener('input', update);    
