19898136921c551e0e8ad5f4b61a345c396a6d91
Opaleckych <adam@opalecky.cz>
2023-11-02 00:19:18 +0100

    feat(JS LOGIC, OUTPUT): Output section, js logic
    Aded output section to bottom of screen, upon entering data state

---
+++ src/components/statemanager.ts
@@ -0,0 +1,122 @@
+import $ from "jquery"
+
+export enum COLORS {
+    WHITE, BLACK
+}
+
+export enum SUBMISSION_TYPE {
+    FLEX, FAIL
+}
+
+export class StateManager {
+    private static __Instance?: StateManager;
+
+    gameLink: string = "";
+    playerName: {
+        ign: string,
+        twitch: string
+    } = {
+        ign: "",
+        twitch: ""
+    };
+    playerColor: COLORS = COLORS.WHITE;
+    submimssionType: SUBMISSION_TYPE = SUBMISSION_TYPE.FLEX;
+    playerMessage: string = "";
+
+    private constructor() {
+        this.registerListeners();
+    }
+
+    private registerListeners() {
+        $("#url").on("input", (e)=>{
+            this.setGameLink($(e.target).val() as string);
+            $(e.target).val(this.gameLink);
+        });
+        $(".color-select button.btn").on("click", (e)=>{
+            $(".color-select button.btn").removeClass("active");
+            $(e.target).addClass("active");
+            this.setColor($(e.target).text().toLowerCase());
+        });
+        $("#ign").on("input", (e)=>this.setIGN($(e.target).val() as string));
+        $("#twitch").on("input", (e)=>this.setTwitchName($(e.target).val() as string));
+        $("#pl-input").on("input", (e)=>this.setPlayerMessage($(e.target).val() as string));
+        $("nav button.btn").on("click", (e)=>{
+            $("nav button.btn").removeClass("active");
+            $(e.target).addClass("active");
+            this.setSubmissionType($(e.target).text().toLowerCase());
+        });
+        $("#submit").on("click", (e)=>{
+            const out = this.exportData();
+            console.log(out);
+            $("#output").text(out);
+        });
+    }
+
+    private setGameLink(newGameLink:string) {
+        if (newGameLink.includes(".com/game/live/")) return this.gameLink = newGameLink.substring(newGameLink.lastIndexOf("/")+1);
+        if (newGameLink.includes("analysis")) return this.gameLink = newGameLink.substring(newGameLink.lastIndexOf("/")+1, newGameLink.indexOf("?"));
+        throw new ReferenceError("Invalid link");
+    }
+
+    private setColor(newColor: string) {
+        switch (newColor) {
+            case "white":
+                this.playerColor = COLORS.WHITE;
+                break;
+            case "black":
+                this.playerColor = COLORS.BLACK;
+                break;
+            default:
+                throw new ReferenceError("Invalid color");
+        }
+    }
+
+    private setTwitchName(newName: string) {
+        this.playerName.twitch = newName;
+    }
+    
+    private setIGN(newName: string) {
+        this.playerName.ign = newName;
+    }
+
+    private setSubmissionType(newType: string) {
+        switch (newType) {
+            case "flex":
+                this.submimssionType = SUBMISSION_TYPE.FLEX;
+                break;
+            case "fail":
+                this.submimssionType = SUBMISSION_TYPE.FAIL;
+                break;
+            default:
+                throw new ReferenceError("Invalid submission type");
+        }
+    }
+
+    private setPlayerMessage(newMessage: string) {
+        this.playerMessage = newMessage;
+    }
+
+    public static Get() {
+        if (!this.__Instance) this.__Instance = new StateManager();
+        return this.__Instance;
+    }
+
+    public exportData() {
+        let out: string = "";
+
+        out += `**[${this.submimssionType == SUBMISSION_TYPE.FLEX ? 'FLEX' : 'FAIL'}]**\n\n`;
+        out += `_I played as ${this.playerColor == COLORS.WHITE ? 'WHITE' : 'BLACK'}_\n\n`;
+
+        for (const paragraph of this.playerMessage.split("\n")) {
+            out+=`> ${paragraph}\n`;
+        }
+        out += "\n";
+
+        if (this.playerName.ign != "") out += `IGN: **${this.playerName.ign}**\n`;
+        if (this.playerName.twitch != "") out += `Twitch: **${this.playerName.ign}**\n`;
+
+        out += `\n_Link to game: https://www.chess.com/game/live/${this.gameLink}_`;
+
+        return out;
+    }    
+}
